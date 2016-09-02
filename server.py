5079ed3cc1a4a04618d619be89dbd979891e7eea
therightman <therightmandev@gmail.com>
2016-09-02 14:24:51 +0100

    I think I started adding the db :P

--- server.py
+++ server.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True)
+    description = db.Column(db.String, unique=True)
+    
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+
+    def __repr__(self):
+        return '<Book %r>' % self.title
 
 @app.route('/')
 def index():
