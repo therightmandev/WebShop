983afd492083e1f64a8ae0d0d295e45edcab0a70
VaggelisD <vagosgan13@gmail.com>
2016-09-07 02:28:09 +0300

    Refactored code and files.
    Refactored code from SCSS files;
    Commented code on SCSS/JS files;
    Changed styling such as coloring;
    Fixed HTML broken links;
    Removed CSS map files;
    Removed the compiled CSS file from it's own folder and placed it inside
    the SASS file, among main.scss;

--- server/static/scripts/main.js
+++ server/static/scripts/main.js
@@ -1,3 +1,4 @@
+// On click of the burger menu, expand it or close it.
 $(".burger-menu").click(function(){
-       $(".hide-mobile").toggle("slide right"); 
+       $(".hide-mobile").toggle("slide right");
 });
